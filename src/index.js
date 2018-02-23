7031677fba91b3a0b39f49fc8b5e264bca4a6d75
ChernyshovaAlexandra <alexandarchrystal@gmail.com>
2018-02-24 01:03:52 +0300

    implementation of function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var symbols = [];
+    var cur = true;
+    for(var i = 0, test; i < str.length; i++){
+        test = false;
+        for(var j = 0; j < bracketsConfig.length; j++){
+            if(str[i] === bracketsConfig[j][0] && str[i] !== symbols[symbols.length - 1]){
+                test = true;
+                symbols.push(bracketsConfig[j][1]);
+            }
+        }
+        if(!test){
+            if(str[i] !== symbols[symbols.length - 1]){
+                cur = false;
+                break;
+            } else {
+                symbols.pop();
+            }
+        }
+    }
+    cur = symbols.length ? false : cur;
+    return cur;
+};
